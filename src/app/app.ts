2e4cf9914c442635c362963b75f060bbf8ea2c0f
Armand Moussaouyi <moussdeve@gmail.com>
2025-11-04 20:45:45 -0700

    Added search field and button

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,21 @@
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    FormsModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('DiFi');
+  searchStore: string = '';
+
+  onSearch(): void {
+    console.log('Searching for:', this.searchStore)
+  }
 }
