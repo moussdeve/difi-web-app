3ff5acd009d03ff936580b33bd404c3b5d502a0a
Armand Moussaouyi <moussdeve@gmail.com>
2025-11-27 15:31:15 -0700

    Adding ngFor to iterate through a list of discount-codes now

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -26,12 +26,38 @@ export class Home {
   searchStore: string = '';
   status: any;
 
-  discountCode: Discountcode = {
-    code: 'WELCOME30',
-    type: '',
-    summary: 'Best for new customers; a free gift is also often included.',
-    discount: '30% Off + Free Shipping'
-  };
+  discountCodeList: Discountcode[] = [
+    {
+      code: 'WELCOME30',
+      type: '',
+      summary: 'Best for new customers; a free gift is also often included.',
+      discount: '30% Off + Free Shipping'
+    },
+    {
+      "code": "WELCOME20",
+      "type": "",
+      "summary": "Another common welcome offer for new customers.",
+      "discount": "20% Off + Free Shipping"
+    },
+    {
+      "code": "CLEAN30",
+      "type": "",
+      "summary": "A general, high-value discount code.",
+      "discount": "30% Off Your Order"
+    },
+    {
+      "code": "TRYGROVE",
+      "type": "",
+      "summary": "Pay only for the shipping of your VIP trial items.",
+      "discount": "Free Starter Kit + Free Shipping"
+    },
+    {
+      "code": "SHIPFREE",
+      "type": "",
+      "summary": "Usually requires a minimum purchase (e.g., $49).",
+      "discount": "Free Shipping on Your Order"
+    }
+ ];
 
   onSearch(): void {
     console.log('Searching for: ', this.searchStore);
